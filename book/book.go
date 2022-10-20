98f92307cfd12e6905c36376822c4bbfaefd4680
romankravchuk <kravchuk.roman02@gmail.com>
2022-10-20 19:46:48 +0800

    add bank heist app

--- book/book.go
+++ book/book.go
@@ -2,22 +2,42 @@ package book
 
 import "fmt"
 
+func RunBookApp() {
+	title, writer := "Mr. GoToSleep", "Tracey Hatchet"
+	artist, publisher := "Jewel Tampson", "DizzyBooks Publishing Inc."
+	ganre := "Romance"
+	year, pageNumber := 1997, 14
+	grade := 6.5
+	cost := calcCost(year, pageNumber, grade)
+
+	showBookInfo(publisher, writer, artist, title, ganre, pageNumber, year, grade, cost)
+
+	title, writer = "Epic Vol. 1", "Ryan N. Shawn"
+	artist = "Phoebe Paperclips"
+	ganre = "Si-Fi"
+	year, pageNumber = 2013, 160
+	grade = 9.0
+	cost = calcCost(year, pageNumber, grade)
+
+	showBookInfo(publisher, writer, artist, title, ganre, pageNumber, year, grade, cost)
+}
+
 // Calculate book price
-func CalcCost(year int, pageNumber int, grade float32) float32 {
-	return float32(2022-year) / 100.00 * float32(pageNumber*100) * grade
+func calcCost(year int, pageNumber int, grade float64) float64 {
+	return float64(2022-year) / 100.00 * float64(pageNumber*100) * grade
 }
 
 // Print book info
-func ShowBookInfo(
-	publisher string,
-	writer string,
-	artist string,
-	title string,
-	pageNumber int,
-	year int,
-	grade float32,
+func showBookInfo(
+	publisher,
+	writer,
+	artist,
+	title,
 	ganre string,
-	cost float32) {
+	pageNumber,
+	year int,
+	grade,
+	cost float64) {
 	fmt.Println(title, "written by", writer, "drawn by", artist)
 	fmt.Println("Pages:", pageNumber, "\nGrade:", grade, "\nGanre:", ganre)
 	fmt.Println("Published by", publisher, "in", year)
--- main.go
+++ main.go
@@ -1,31 +1,14 @@
 package main
 
 import (
+	bankheist "github.com/romankravchuk/learn-go/bank-heist"
 	"github.com/romankravchuk/learn-go/book"
+	"github.com/romankravchuk/learn-go/gopher"
 )
 
 // Start program point
 func main() {
-	// showGopher()
-	var publisher, writer, artist, title, ganre string
-	var year, pageNumber int
-	var grade, cost float32
-
-	title, writer = "Mr. GoToSleep", "Tracey Hatchet"
-	artist, publisher = "Jewel Tampson", "DizzyBooks Publishing Inc."
-	ganre = "Romance"
-	year, pageNumber = 1997, 14
-	grade = 6.5
-	cost = book.CalcCost(year, pageNumber, grade)
-
-	book.ShowBookInfo(publisher, writer, artist, title, pageNumber, year, grade, ganre, cost)
-
-	title, writer = "Epic Vol. 1", "Ryan N. Shawn"
-	artist = "Phoebe Paperclips"
-	ganre = "Si-Fi"
-	year, pageNumber = 2013, 160
-	grade = 9.0
-	cost = book.CalcCost(year, pageNumber, grade)
-
-	book.ShowBookInfo(publisher, writer, artist, title, pageNumber, year, grade, ganre, cost)
+	gopher.PrintGopher()
+	book.RunBookApp()
+	bankheist.RunBankHeist()
 }
