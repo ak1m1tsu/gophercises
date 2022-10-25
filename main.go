98b54d60abc792502c7a2217713057e26a31a9e3
romankravchuk <kravchuk.roman02@gmail.com>
2022-10-25 19:46:04 +0800

    fix: main func

--- main.go
+++ main.go
@@ -1,14 +1,5 @@
 package main
 
-import (
-	bankheist "github.com/romankravchuk/learn-go/bank-heist"
-	"github.com/romankravchuk/learn-go/book"
-	"github.com/romankravchuk/learn-go/gopher"
-)
-
-// Start program point
 func main() {
-	gopher.PrintGopher()
-	book.RunBookApp()
-	bankheist.RunBankHeist()
+
 }
