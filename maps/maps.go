f9314e2dec286e432231d1478ce37c80a8e1eb2c
romankravchuk <kravchuk.roman02@gmail.com>
2022-10-27 17:25:46 +0800

    feat: add PrintColors

--- maps/maps.go
+++ maps/maps.go
@@ -3,12 +3,15 @@ package maps
 import "fmt"
 
 func GetColors() map[string]string {
-	colors := map[string]string{
+	return map[string]string{
 		"red":   "#ff00000",
 		"green": "#4bf745",
 		"white": "#ffffff",
 	}
-
-	return colors
 }
 
+func PrintColors(colors map[string]string) {
+	for color, hex := range colors {
+		fmt.Printf("Hex code for %v is %v\n", color, hex)
+	}
+}
