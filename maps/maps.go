29679ea656bb79b729a2b9fb9774b1ac03e7bded
romankravchuk <kravchuk.roman02@gmail.com>
2022-10-27 17:23:09 +0800

    feat: add GetColor

---
+++ maps/maps.go
@@ -0,0 +1,14 @@
+package maps
+
+import "fmt"
+
+func GetColors() map[string]string {
+	colors := map[string]string{
+		"red":   "#ff00000",
+		"green": "#4bf745",
+		"white": "#ffffff",
+	}
+
+	return colors
+}
+
